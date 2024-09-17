62ab960c1ea442157bc3664ea43b1bddc400b391
Striveonger <striveonger@163.com>
2024-09-17 10:01:07 +0800

    add isTextFile

--- own-core/src/main/java/com/striveonger/common/core/utils/FileUtil.java
+++ own-core/src/main/java/com/striveonger/common/core/utils/FileUtil.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -14,13 +15,38 @@ import java.net.URL;
  * @since 2023-06-17 18:28
  */
 public class FileUtil {
-    private final Logger log = LoggerFactory.getLogger(FileUtil.class);
+    private static final Logger log = LoggerFactory.getLogger(FileUtil.class);
 
+    /**
+     * 获取文件的URL
+     * @param file
+     * @return
+     */
     public static URL toURL(File file) {
         try {
             return file.toURI().toURL();
         } catch (MalformedURLException e) {
             throw new CustomException(ResultStatus.ACCIDENT, String.format("Illegal [%s] to URL", file));
+        }
+    }
+
+    /**
+     * 判断文件是否是文本文件
+     * @param file
+     * @return
+     */
+    public static boolean isTextFile(File file) {
+        try (FileInputStream fis = new FileInputStream(file)) {
+            int byteRead;
+            while ((byteRead = fis.read())!= -1) {
+                if (byteRead < 0x20 && byteRead!= 0x09 && byteRead!= 0x0A && byteRead!= 0x0D) {
+                    return false;
+                }
+            }
+            return true;
+        } catch (Exception e) {
+            log.error("check file type error...", e);
+            return false;
         }
     }
 }
